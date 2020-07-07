9c286282059733ff8c7486dae7de87841ccb5dcc
dennis-phillips <dennis_phillips@outlook.com>
2020-07-07 12:23:20 -0400

    walkthruogh continued

--- CodingEventsDemo/Models/Event.cs
+++ CodingEventsDemo/Models/Event.cs
@@ -14,15 +14,9 @@ namespace CodingEventsDemo.Models
         public EventType Type { get; set; }
 
         public int Id { get; }
-        static private int nextId = 1;
+        
 
-        public Event()
-        {
-            Id = nextId;
-            nextId++;
-        }
-
-        public Event(string name, string description, string contactEmail) : this()
+        public Event(string name, string description, string contactEmail) 
         {
             Name = name;
             Description = description;
